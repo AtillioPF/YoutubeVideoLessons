79bd991a80cb28547d60348ba692d56ad7b997d8
Atillio Pinno Fetter <atilliopf@hotmail.com>
2023-01-29 14:36:09 -0300

    finished implementing linked_list class

--- LinkedLists/LinkedList.py
+++ LinkedLists/LinkedList.py
@@ -11,16 +11,23 @@ class linked_list():
         
     def append(self,data):
         new_node = node(data)
-        cur = self.head
-        while cur.next!=None:
-            cur = cur.next
-        cur.next = new_node
+        cur_node = self.head
+        while cur_node.next!=None:
+            cur_node = cur_node.next
+        cur_node.next = new_node
         
     def length(self):
-        cur = self.head
+        cur_node = self.head
         total = 0
-        while cur.next!=None:
+        while cur_node.next!=None:
             total+=1
-            cur=cur.next
+            cur_node=cur_node.next
         return total
     
+    def display(self):
+        elems = []
+        cur_node = self.head
+        while cur_node.next!=None:
+            cur_node = cur_node.next
+            elems.append(cur_node.data)
+        print(elems)
